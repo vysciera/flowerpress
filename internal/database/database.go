1d74cb216150182a160d3a79b32c62ae1fde6e2a
dirac <hobbiathan@gmail.com>
2026-09-01 07:10:18 -0600

    Open(), ensureDirectory()

---
+++ internal/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import(
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "turso.tech/database/tursogo"
+)
+
+func Open(path string) (*sql.DB, error) {
+	if err := ensureDirectory(path); err != nil {
+		return nil, fmt.Errorf("create database directory: %w", err)
+	}
+
+	db, err := sql.Open("turso", path)
+	if err != nil {
+		db.Close()
+		return nil, fmt.Errorf("open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("ping database: %w", err)
+	}
+
+	return db, nil
+}
+
+func ensureDirectory(path string) error {
+	dir := filepath.Dir(path)
+
+	if dir == "." {
+		return nil
+	}
+
+	return os.MkdirAll(dir, 0755)
+}
